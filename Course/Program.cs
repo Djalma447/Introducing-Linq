c8e0ae99c174c9dd358b330f5a30117bd4abfed2
Djalma <djalma.ant@gmail.com>
2020-05-06 01:09:31 -0300

    Linq Sample using List AND ToList

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Course
 {
@@ -6,7 +8,16 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Specify the data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the query expression
+            List<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10).ToList();
+
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
